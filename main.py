0926ee1dc073cdbc9a19b8832ea349d612c97358
DipendraTamang <dipendra.tamang9@gmail.com>
2022-04-22 15:52:22 +0545

    succesl

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-import pyautogui, time
+
+import pyautogui,time
 time.sleep(5)
 f = open("text.txt", 'r')
 for word in f:
     pyautogui.typewrite(word)
     pyautogui.press("enter")
+
+# import pyautogui as spam
+# import time
+#
+# time.sleep(5)
+#
+# limit = input("enter a limit message")
+# msg = input("enter a message")
+#
+# i=0
+# while i < int(limit):
+#     spam.typewrite(msg)
+#     spam.press("enter")
+#     i +=1
